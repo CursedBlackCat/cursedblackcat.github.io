8c6f874c98a7e4b407797e02277c323037e53e77
CursedBlackCat <17520254+CursedBlackCat@users.noreply.github.com>
2025-01-30 02:59:53 +0900

    Add dynamic DX rating plates and trophy case text

---
+++ dxrating.js
@@ -0,0 +1,31 @@
+const dxRating = 12857;
+
+window.onload = function() {
+    let ratingPlate = document.getElementById("maimai-rating-plate");
+    let ratingNumber = document.getElementById("maimai-rating-number");
+    let ratingColour = document.getElementById("maimai-rating-colour");
+
+    ratingNumber.innerText = dxRating.toString();
+
+    if (dxRating >= 15000){
+        ratingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_rainbow.png\")"
+        ratingColour.innerText = "Rainbow";
+    } else if (dxRating >= 14000) {
+        ratingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_gold.png\")"
+        ratingColour.innerText = "Gold";
+    } else if (dxRating >= 13000) {
+        ratingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_silver.png\")"
+        ratingColour.innerText = "Silver";
+    } else if (dxRating >= 12000) {
+        ratingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_bronze.png\")";
+        ratingColour.innerText = "Bronze";
+    } else if (dxRating >= 10000) {
+        ratingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_purple.png\")"
+        ratingColour.innerText = "Purple";
+    } else if (dxRating >= 8000) {
+        ratingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_red.png\")"
+        ratingColour.innerText = "Red";
+    } else {
+        ratingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_blue.png\")"
+    }
+};
