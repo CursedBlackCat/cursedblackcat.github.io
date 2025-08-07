e06094500113fe06f9b7f8dd5db90b5c981c4db7
Darren Yip <17520254+CursedBlackCat@users.noreply.github.com>
2025-08-07 16:14:30 +0900

    Split maimai DX rating in trophy case to current and peak DX rating

--- dxrating.js
+++ dxrating.js
@@ -1,34 +1,66 @@
-const dxRating = 14631;
+const peakDxRating = 14631;
+const currentDxRating = 14476;
 
 window.addEventListener("load",function() {
-    let ratingPlate = document.getElementById("maimai-rating-plate");
-    let ratingNumber = document.getElementById("maimai-rating-number");
-    let ratingColour = document.getElementById("maimai-rating-colour");
+    let peakRatingPlate = document.getElementById("maimai-peak-rating-plate");
+    let peakRatingNumber = document.getElementById("maimai-peak-rating-number");
+    let peakRatingColour = document.getElementById("maimai-peak-rating-colour");
+    let currentRatingPlate = document.getElementById("maimai-current-rating-plate");
+    let currentRatingNumber = document.getElementById("maimai-current-rating-number");
+    let currentRatingColour = document.getElementById("maimai-current-rating-colour");
 
-    ratingNumber.innerText = dxRating.toString();
+    peakRatingNumber.innerText = peakDxRating.toString();
 
-    if (dxRating >= 15000){
-        ratingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_rainbow.png\")"
-        ratingColour.innerText = "Rainbow";
-    } else if (dxRating >= 14500) {
-        ratingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_platinum.png\")"
-        ratingColour.innerText = "Platinum";
-    } else if (dxRating >= 14000) {
-        ratingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_gold.png\")"
-        ratingColour.innerText = "Gold";
-    } else if (dxRating >= 13000) {
-        ratingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_silver.png\")"
-        ratingColour.innerText = "Silver";
-    } else if (dxRating >= 12000) {
-        ratingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_bronze.png\")";
-        ratingColour.innerText = "Bronze";
-    } else if (dxRating >= 10000) {
-        ratingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_purple.png\")"
-        ratingColour.innerText = "Purple";
-    } else if (dxRating >= 7000) {
-        ratingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_red.png\")"
-        ratingColour.innerText = "Red";
+    if (peakDxRating >= 15000){
+        peakRatingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_rainbow.png\")"
+        peakRatingColour.innerText = "Rainbow";
+    } else if (peakDxRating >= 14500) {
+        peakRatingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_platinum.png\")"
+        peakRatingColour.innerText = "Platinum";
+    } else if (peakDxRating >= 14000) {
+        peakRatingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_gold.png\")"
+        peakRatingColour.innerText = "Gold";
+    } else if (peakDxRating >= 13000) {
+        peakRatingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_silver.png\")"
+        peakRatingColour.innerText = "Silver";
+    } else if (peakDxRating >= 12000) {
+        peakRatingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_bronze.png\")";
+        peakRatingColour.innerText = "Bronze";
+    } else if (peakDxRating >= 10000) {
+        peakRatingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_purple.png\")"
+        peakRatingColour.innerText = "Purple";
+    } else if (peakDxRating >= 7000) {
+        peakRatingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_red.png\")"
+        peakRatingColour.innerText = "Red";
     } else {
-        ratingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_blue.png\")"
+        peakRatingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_blue.png\")"
     }
+
+    currentRatingNumber.innerText = currentDxRating.toString();
+
+    if (currentDxRating >= 15000){
+        currentRatingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_rainbow.png\")"
+        currentRatingColour.innerText = "Rainbow";
+    } else if (currentDxRating >= 14500) {
+        currentRatingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_platinum.png\")"
+        currentRatingColour.innerText = "Platinum";
+    } else if (currentDxRating >= 14000) {
+        currentRatingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_gold.png\")"
+        currentRatingColour.innerText = "Gold";
+    } else if (peakDxRating >= 13000) {
+        currentRatingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_silver.png\")"
+        currentRatingColour.innerText = "Silver";
+    } else if (currentDxRating >= 12000) {
+        currentRatingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_bronze.png\")";
+        currentRatingColour.innerText = "Bronze";
+    } else if (currentDxRating >= 10000) {
+        currentRatingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_purple.png\")"
+        currentRatingColour.innerText = "Purple";
+    } else if (currentDxRating >= 7000) {
+        currentRatingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_red.png\")"
+        currentRatingColour.innerText = "Red";
+    } else {
+        currentRatingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_blue.png\")"
+    }
+
 }, false);
