3c909a96f893a1e29e3168dfcb8355697344d62d
CursedBlackCat <17520254+CursedBlackCat@users.noreply.github.com>
2025-09-15 02:18:43 +0900

    DX rating 14592 -> 14665, comment out peak rating, fix incorrect PRiSM ending rating

--- dxrating.js
+++ dxrating.js
@@ -1,40 +1,12 @@
-const currentDxRating = 14592;
+const currentDxRating = 14665;
 const peakDxRating = 14644;
 
+const showOldPeakRating = false; // Remember to uncomment the HTML elements if setting this to true
+
 window.addEventListener("load",function() {
-    let peakRatingPlate = document.getElementById("maimai-peak-rating-plate");
-    let peakRatingNumber = document.getElementById("maimai-peak-rating-number");
-    let peakRatingColour = document.getElementById("maimai-peak-rating-colour");
     let currentRatingPlate = document.getElementById("maimai-current-rating-plate");
     let currentRatingNumber = document.getElementById("maimai-current-rating-number");
     let currentRatingColour = document.getElementById("maimai-current-rating-colour");
-
-    peakRatingNumber.innerText = peakDxRating.toString();
-
-    if (peakDxRating >= 15000){
-        peakRatingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_rainbow.png\")"
-        peakRatingColour.innerText = "Rainbow";
-    } else if (peakDxRating >= 14500) {
-        peakRatingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_platinum.png\")"
-        peakRatingColour.innerText = "Platinum";
-    } else if (peakDxRating >= 14000) {
-        peakRatingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_gold.png\")"
-        peakRatingColour.innerText = "Gold";
-    } else if (peakDxRating >= 13000) {
-        peakRatingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_silver.png\")"
-        peakRatingColour.innerText = "Silver";
-    } else if (peakDxRating >= 12000) {
-        peakRatingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_bronze.png\")";
-        peakRatingColour.innerText = "Bronze";
-    } else if (peakDxRating >= 10000) {
-        peakRatingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_purple.png\")"
-        peakRatingColour.innerText = "Purple";
-    } else if (peakDxRating >= 7000) {
-        peakRatingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_red.png\")"
-        peakRatingColour.innerText = "Red";
-    } else {
-        peakRatingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_blue.png\")"
-    }
 
     currentRatingNumber.innerText = currentDxRating.toString();
 
@@ -63,4 +35,35 @@ window.addEventListener("load",function() {
         currentRatingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_blue.png\")"
     }
 
+    if (showOldPeakRating) {
+        let peakRatingPlate = document.getElementById("maimai-peak-rating-plate");
+        let peakRatingNumber = document.getElementById("maimai-peak-rating-number");
+        let peakRatingColour = document.getElementById("maimai-peak-rating-colour");
+        peakRatingNumber.innerText = peakDxRating.toString();
+
+        if (peakDxRating >= 15000){
+            peakRatingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_rainbow.png\")"
+            peakRatingColour.innerText = "Rainbow";
+        } else if (peakDxRating >= 14500) {
+            peakRatingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_platinum.png\")"
+            peakRatingColour.innerText = "Platinum";
+        } else if (peakDxRating >= 14000) {
+            peakRatingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_gold.png\")"
+            peakRatingColour.innerText = "Gold";
+        } else if (peakDxRating >= 13000) {
+            peakRatingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_silver.png\")"
+            peakRatingColour.innerText = "Silver";
+        } else if (peakDxRating >= 12000) {
+            peakRatingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_bronze.png\")";
+            peakRatingColour.innerText = "Bronze";
+        } else if (peakDxRating >= 10000) {
+            peakRatingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_purple.png\")"
+            peakRatingColour.innerText = "Purple";
+        } else if (peakDxRating >= 7000) {
+            peakRatingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_red.png\")"
+            peakRatingColour.innerText = "Red";
+        } else {
+            peakRatingPlate.style.backgroundImage = "url(\"images/maimai/rating_base_blue.png\")"
+        }
+    }
 }, false);
