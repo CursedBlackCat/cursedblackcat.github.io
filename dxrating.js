6854274bd9ed89f0db893d6e1d68abc4dea5fc0e
Darren Yip <17520254+CursedBlackCat@users.noreply.github.com>
2025-10-27 13:49:40 +0900

    DX rating 14913 -> 14930

--- dxrating.js
+++ dxrating.js
@@ -1,5 +1,5 @@
-const currentDxRating = 14913;
-const peakDxRating = 14913;
+const currentDxRating = 14930;
+const peakDxRating = 14930;
 
 const showOldPeakRating = false; // Remember to uncomment the HTML elements if setting this to true
 
